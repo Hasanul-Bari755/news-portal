5e2b516fd2bcc9a4919a3b03d4df8346a1f68aa3
Hasanul Bari <hasanulbari8@gmail.com>
2022-09-03 01:35:46 +0600

    modal data load

--- js/news.js
+++ js/news.js
@@ -31,20 +31,20 @@ const loadCatagoryDetailsDisplay = (datas) =>{
      const cardContainer = document.getElementById('card-container');
      cardContainer.innerHTML = "";
      datas.forEach(data => {
-        console.log(data)
-        const {thumbnail_url,title,details,author,rating} = data;
+      //  console.log(data)
+        const {thumbnail_url,title,details,author,rating,_id} = data;
         const {img,name} = author;
         const{number} = rating;
         const div = document.createElement('div');
       //  div.classList.add("row g-0");
-      div.setAttribute("class","row g-0 mt-2 p-2 ")
+      div.setAttribute("class","row g-0 mt-2 p-3 shadow ")
         div.innerHTML = `
         <div class="col-md-4">
         <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">${title}</h5>
+          <h5 class="card-title fw-bold">${title}</h5>
           <p class="card-text">${details}</p>
           <div class="d-flex justify-content-between">
                <div class="d-flex">
@@ -52,10 +52,10 @@ const loadCatagoryDetailsDisplay = (datas) =>{
                   <p class="ms-3">${name}</p>
                </div>
                <div>
-                   <p>${number}</p>
+                   <p class="fw-bold">${number}</p>
                </div>
                <div>
-               <button type="button" class="btn btn-info">Details</button>
+               <button onclick="modalLoadData('${_id}')" type="button" class="btn btn-info fw-bold">Details</button>
                </div>
             </div>
          </div>
@@ -64,5 +64,11 @@ const loadCatagoryDetailsDisplay = (datas) =>{
         `;
         cardContainer.appendChild(div);
      });
+}
+
+const modalLoadData = async(id)=>{
+   const response = await fetch(`https://openapi.programming-hero.com/api/news/${id}`);
+   const data = await response.json();
+   console.log(data.data[0])
 }
 loadAllCategory()
