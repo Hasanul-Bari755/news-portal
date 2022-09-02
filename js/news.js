b784c577931d9435b2a6c710fe2113ae9752be35
Hasanul Bari <hasanulbari8@gmail.com>
2022-09-03 01:01:03 +0600

    update

--- js/news.js
+++ js/news.js
@@ -21,9 +21,39 @@ const loadAllCategoryShow = (datas) =>{
    });
 }
 
-const loadCatagoryDetails = async (id) =>{
+const loadCatagoryDetails = async (id) =>{ 
    const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`);
    const data  = await response.json();
-   console.log(data.data)
+   loadCatagoryDetailsDisplay(data.data)
+}
+
+const loadCatagoryDetailsDisplay = (datas) =>{
+     const cardContainer = document.getElementById('card-container');
+     cardContainer.innerHTML = "";
+     datas.forEach(data => {
+        console.log(data)
+        const {thumbnail_url,title,details,author} = data;
+        const {img,name} = author;
+        const div = document.createElement('div');
+      //  div.classList.add("row g-0");
+      div.setAttribute("class","row g-0 mt-2 p-2 ")
+        div.innerHTML = `
+        <div class="col-md-4">
+        <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${details}</p>
+          <div class="d-flex justify-content-between">
+          <img src="${img}" class="rounded-circle img" alt="...">
+          <p>${name}</p>
+          </div>
+         </div>
+      </div>
+     
+        `;
+        cardContainer.appendChild(div);
+     });
 }
 loadAllCategory()
